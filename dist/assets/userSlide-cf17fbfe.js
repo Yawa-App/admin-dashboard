4770c0348ebefb455ace0d61abc384614292891d
TEMPLE VOKE <62543994+temple7447@users.noreply.github.com>
2024-11-21 13:35:12 +0100

    added

---
+++ dist/assets/userSlide-cf17fbfe.js
@@ -0,0 +1,1 @@
+import{a2 as s}from"./index-a9286523.js";const i=s.injectEndpoints({endpoints:e=>({getUsers:e.query({query:()=>({url:"/api/admin/users",method:"GET",params:{page:1,limit:30}})})})}),{useGetUsersQuery:r}=i;export{r as u};
